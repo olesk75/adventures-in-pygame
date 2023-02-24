a06a9f3e1570c72fbbf9f9f3db9dfa0ac2e717c1
olesk75 <olesk@pvv.org>
2023-02-24 17:36:45 +0100

    Added arena sign

--- lvl/Tiled - levels/Decorations.tsx
+++ lvl/Tiled - levels/Decorations.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Decorations" tilewidth="32" tileheight="32" tilecount="5" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="Decorations" tilewidth="640" tileheight="256" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="1">
   <image width="32" height="32" source="../../assets/tile/decorations/2.png"/>
@@ -16,4 +16,7 @@
  <tile id="5">
   <image width="32" height="32" source="../../assets/tile/decorations/6.png"/>
  </tile>
-	</tileset>
+ <tile id="6">
+  <image width="640" height="256" source="../../assets/tile/decorations/7.png"/>
+ </tile>
+</tileset>
